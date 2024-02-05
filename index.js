1af47e0df99b90e4b17647d4a690c885d4b53639
Lucas Eduardo <Lucaspity721@gmail.com>
2024-02-04 23:28:17 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { Recorder, FileHandler } = require("node-rtsp-recorder");
+const { FileHandler } = require("node-rtsp-recorder");
 const { Stream } = require("./src/helpers/stream");
+const RTSPRecorder = require("node-rtsp-recorder/src/helpers/recorder");
 
 module.exports = {
-  Recorder: Recorder,
+  Recorder: RTSPRecorder,
   FileHandler: FileHandler,
   Stream: Stream
 }
